07fd8de11be84ee8fb46bb1c1e814e0d1df10f61
Ash Spungin <bigspungin@gmail.com>
2019-02-12 11:56:43 -0800

    shape

--- project0.js
+++ project0.js
@@ -1,7 +1,46 @@
 function setup() {
 
+createCanvas (300,300);
+background (200);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+
+
+
+   beginShape();
+   vertex (10,300);
+   vertex (10,280);
+   vertex (20, 280);
+   vertex (20, 260);
+   vertex (30, 260);
+   vertex (30, 240);
+   vertex (40, 240);
+   vertex (40, 220);
+   vertex (50, 220);
+   vertex (50, 200);
+   vertex (60, 200);
+   vertex (60, 180);
+   vertex (70, 180);
+   vertex (70, 160);
+   vertex (80, 160);
+   vertex (80, 140);
+   vertex (90, 140);
+   vertex (90, 120);
+   vertex (100, 120);
+   vertex (100, 100);
+   vertex (110, 100);
+   vertex (110, 80);
+   vertex (120, 80);
+   vertex (120, 60);
+   vertex (130, 60);
+   vertex (130, 40);
+   vertex (140, 40);
+   vertex (140, 20);
+   vertex (150, 20);
+   vertex (200, 300);
+   vertex (10,300);
+   endShape (fill (0));
+
+ 
 }
